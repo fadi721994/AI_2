8b21d3a4def445ac036fa2b27908471ddd704de9
Fadi Yousef <fadi721994@gmail.com>
2019-04-02 01:32:34 +0300

    First

--- utils.py
+++ utils.py
@@ -94,7 +94,7 @@ def calc_avg(data_list):
 def parse_cmd():
     parser = argparse.ArgumentParser()
     parser.add_argument('-T', default=15, help='Time limit. Default is 15 seconds.')
-    parser.add_argument('-A', default=3, help='Algorithm number: 0 for A star (default), 1 for IDA star, 2 for '
+    parser.add_argument('-A', default=0, help='Algorithm number: 0 for A star (default), 1 for IDA star, 2 for '
                                               'bi-directional A star, 3 for reinforcement learning.')
     parser.add_argument('-I', default=0, help='Indicator number: 0 for \'No indicator\' (default), 1 for '
                                               '\'Board freedom degree\', 2 for \'Overall free cars\', 3 for both '
